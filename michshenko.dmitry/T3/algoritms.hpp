62ecabf75bfcf602990a99dfefc189f549670a39
PILIGRIM <dimamishenko2006@gmail.com>
2026-05-21 06:26:19 +0300

    final version

--- michshenko.dmitry/T3/algoritms.hpp
+++ michshenko.dmitry/T3/algoritms.hpp
@@ -1,24 +1,25 @@
-#ifndef ALGORIMS_HPP
-#define ALGORIMS_HPP
+#ifndef ALGORITMS_HPP
+#define ALGORITMS_HPP
 
-#include <vector>
 #include <string>
-
+#include <vector>
 #include "Polygon.hpp"
 
-// методы
+// AREA
 double areaEvenOdd(const std::string& value, const std::vector<Polygon>& pols);
 double areaMean(const std::vector<Polygon>& pols);
 double areaNum(int value, const std::vector<Polygon>& pols);
+
+// MAX / MIN
 double maxAreaVertex(const std::string& value, const std::vector<Polygon>& pols);
 double minAreaVertex(const std::string& value, const std::vector<Polygon>& pols);
+
+// COUNT
 int countVertexOddNum(const std::string& value, const std::vector<Polygon>& pols);
-int countVertexOddNum(const size_t& value, const std::vector<Polygon>& pols);
+int countVertexOddNum(size_t value, const std::vector<Polygon>& pols);
 
-// проверка на принадлежность фигуры целиком в прямоугольнике
+// INFRAME / MAXSEQ
 bool isInframe(const Polygon& pol, const std::vector<Polygon>& pols);
-
-// максимальная последовательность одинаковых фигур
 int maxSeq(const std::vector<Polygon>& pols, const Polygon& target);
 
 #endif
